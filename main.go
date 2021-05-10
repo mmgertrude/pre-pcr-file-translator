e8bff33d9242efafdf5519c286c34efac9d56b72
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2021-05-10 22:12:35 +0200

    create file discoverer function

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//source: https://stackoverflow.com/questions/45578172/golang-find-most-recent-file-by-date-and-time
 package main
 
 import (
@@ -5,62 +6,34 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"time"
 )
 
+//global variables:
+var path = "./testfolder/"
+
 func main() {
-	check_dir()
-	/*
-		folder_to_watch := "./testfolder/"
-		file_to_Check := "testfile"
-
-		filename, modifiedtime := new_file_finder(file_to_Check)
-		fmt.Printf("File %s was last modified %s \n", filename, modifiedtime)
-
-		path, modifiedtime := find_new_file(folder_to_watch)
-		fmt.Printf("path %s was last modified %s \n", path, modifiedtime)
-	*/
+	newestFile := new_file_discover()
+	fmt.Println(newestFile)
 }
 
-func check_dir() {
-	files, err := ioutil.ReadDir("./testfolder")
+func new_file_discover() string {
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, file := range files {
-		fmt.Println(file.Name(), " ", file.ModTime(), " ", file.ModTime().Unix())
-
+	var newestFile string
+	var newestTime int64 = 0
+	for _, f := range files {
+		fi, err := os.Stat(path + f.Name())
+		if err != nil {
+			fmt.Println(err)
+		}
+		currTime := fi.ModTime().Unix()
+		if currTime > newestTime {
+			newestTime = currTime
+			newestFile = f.Name()
+		}
 	}
-}
-
-func find_new_file(path string) (string, time.Time) {
-	// get last modified time
-	file, err := os.Stat(path)
-	if err != nil {
-		//fmt.Println("there was an error \n", err)
-		//fmt.Print("---------------")
-		log.Fatal(err)
-	}
-
-	modifiedtime := file.ModTime()
-	return path, modifiedtime
-
-}
-
-func new_file_finder(new_file string) (string, time.Time) {
-
-	filename := new_file //file with newest modification date
-
-	// get last modified time
-	file, err := os.Stat(filename)
-	if err != nil {
-		//fmt.Println("there was an error \n", err)
-		//fmt.Print("---------------")
-		log.Fatal(err)
-	}
-
-	modifiedtime := file.ModTime()
-	return filename, modifiedtime
+	return (newestFile)
 
 }
